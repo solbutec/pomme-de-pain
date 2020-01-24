74baf88596eeade0bc6ebf23147ed6e4431a4f2d
MJID <a.elhamdaoui@karizma.ma>
2020-01-24 16:17:28 +0100

    [FIX] Errors views: pos_user_controll V11.0

--- pos_user_control/static/src/js/models.js
+++ pos_user_control/static/src/js/models.js
@@ -10,8 +10,10 @@ odoo.define('pos_user_control.models', function (require) {
         if (model.model === 'res.users') {
             model.fields.push('has_pos_price_control');
             model.fields.push('has_pos_qty_control');
+            model.fields.push('has_pos_discount_control');
             model.fields.push('has_pos_back_backspace_control');
             model.fields.push('has_pos_delete_order_control');
+            
         }
     }
 
@@ -19,6 +21,7 @@ odoo.define('pos_user_control.models', function (require) {
 
         init: function (parent, options) {
             this._super(parent, options);
+            this.applyAccessRights();
         },
 
         applyAccessRights: function () {
@@ -26,35 +29,117 @@ odoo.define('pos_user_control.models', function (require) {
             var user = this.pos.get_cashier();
             var has_pos_price_control = user.has_pos_price_control;
             var has_pos_qty_control = user.has_pos_qty_control;
+            var has_pos_discount_control = user.has_pos_discount_control;
             var has_pos_back_backspace_control = user.has_pos_back_backspace_control;
+            var has_pos_delete_order_control = user.has_pos_delete_order_control;
+
             this.$el.find('.mode-button[data-mode="price"]')
                 .toggleClass('disabled-mode', !has_pos_price_control)
                 .prop('disabled', !has_pos_price_control);
+
             this.$el.find('.mode-button[data-mode="quantity"]')
                 .toggleClass('disabled-mode', !has_pos_qty_control)
                 .prop('disabled', !has_pos_qty_control);
+
             this.$el.find('.mode-button[data-mode="quantity"]')
                 .toggleClass('selected-mode', has_pos_qty_control);
+
             this.$el.find('.zero').toggleClass('disabled-mode', !has_pos_qty_control)
                 .prop('disabled', !has_pos_qty_control);
+            this.$el.find('.numpad-minus').toggleClass('disabled-mode', !has_pos_qty_control)
+                .prop('disabled', !has_pos_qty_control);
+
+            this.$el.find('.mode-button[data-mode="discount"]').toggleClass('disabled-mode', !has_pos_discount_control)
+                .prop('disabled', !has_pos_discount_control);
+
+
             this.$el.find('.numpad-backspace')
                 .toggleClass('disabled-mode', !has_pos_back_backspace_control)
                 .prop('disabled', !has_pos_back_backspace_control);
+
+            this.$el.find('.deleteorder-button')
+                .toggleClass('disabled-mode', !has_pos_delete_order_control)
+                .prop('disabled', !has_pos_delete_order_control);
+
+
+                
         },
 
         clickChangeMode: function (event) {
             this._super.apply(this, arguments);
             var user = this.pos.get_cashier();
             var has_pos_qty_control = user.has_pos_qty_control;
+            var has_pos_price_control = user.has_pos_price_control;
+            var has_pos_back_backspace_control = user.has_pos_back_backspace_control;
+
             this.$el.find('.zero').toggleClass('disabled-mode', !has_pos_qty_control)
+                .prop('disabled', !has_pos_qty_control);
+            this.$el.find('.numpad-minus').toggleClass('disabled-mode', !has_pos_qty_control)
                 .prop('disabled', !has_pos_qty_control);
             var newMode = event.currentTarget.attributes['data-mode'].nodeValue;
             if (newMode === 'discount'){
                 var num_zero = this.$el.find('.zero');
                 num_zero.removeClass('disabled-mode');
-                num_zero.removeProp('disabled')
+                num_zero.removeProp('disabled');
             }
         }
 
     });
+
+    //set_cashier(user)
+    var _super_posModel = models.PosModel;
+     models.PosModel = models.PosModel.extend({
+        set_cashier: function(user){
+            _super_posModel.prototype.set_cashier.call(this, usser);
+            this.applyAccessRights();
+
+        },
+
+         applyAccessRights: function () {
+            this._super();
+            var user = this.get_cashier();
+            var has_pos_price_control = user.has_pos_price_control;
+            var has_pos_qty_control = user.has_pos_qty_control;
+            var has_pos_discount_control = user.has_pos_discount_control;
+            var has_pos_back_backspace_control = user.has_pos_back_backspace_control;
+            var has_pos_delete_order_control = user.has_pos_delete_order_control;
+
+            this.$el.find('.mode-button[data-mode="price"]')
+                .toggleClass('disabled-mode', !has_pos_price_control)
+                .prop('disabled', !has_pos_price_control);
+
+            this.$el.find('.mode-button[data-mode="quantity"]')
+                .toggleClass('disabled-mode', !has_pos_qty_control)
+                .prop('disabled', !has_pos_qty_control);
+
+            this.$el.find('.mode-button[data-mode="quantity"]')
+                .toggleClass('selected-mode', has_pos_qty_control);
+
+            this.$el.find('.zero').toggleClass('disabled-mode', !has_pos_qty_control)
+                .prop('disabled', !has_pos_qty_control);
+            this.$el.find('.numpad-minus').toggleClass('disabled-mode', !has_pos_qty_control)
+                .prop('disabled', !has_pos_qty_control);
+
+            this.$el.find('.mode-button[data-mode="discount"]').toggleClass('disabled-mode', !has_pos_discount_control)
+                .prop('disabled', !has_pos_discount_control);
+
+
+            this.$el.find('.numpad-backspace')
+                .toggleClass('disabled-mode', !has_pos_back_backspace_control)
+                .prop('disabled', !has_pos_back_backspace_control);
+
+            this.$el.find('.deleteorder-button')
+                .toggleClass('disabled-mode', !has_pos_delete_order_control)
+                .prop('disabled', !has_pos_delete_order_control);
+
+
+                
+        },
+
+
+
+
+        });
+
+
 });
