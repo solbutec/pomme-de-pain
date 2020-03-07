2950bdfc62069b6fb26a50598a1e70575fe1440d
MJID: personnal-PC notebook <abdelmajidelhamdaoui@gmail.com>
2020-03-07 19:54:32 +0100

    [PREP] Reporting wizard and qweb template

--- pdp_reporting/wizards/reporting_wizard.py
+++ pdp_reporting/wizards/reporting_wizard.py
@@ -5,6 +5,12 @@ from odoo.exceptions import UserError
 from datetime import datetime
 from pprint import pprint
 
+REPORT_TITLES = {
+    'main_ouvre_glob': "Main courante global", 
+    'main_ouvre_cais': "Main courante (Caissier)", 
+    'vente_eclat': "Ventes éclatées",
+    'vente_non_eclat': "Ventes non éclatées",
+}
 class PosConfigWizard(models.TransientModel):
     _name = 'pos.config.reporting.wizard'
 
@@ -17,9 +23,12 @@ class PosConfigWizard(models.TransientModel):
 
         return date_start
 
+    def get_report_title(self):
+        return REPORT_TITLES[self.type] if self.type else '-'
 
-    debut_date = fields.Datetime("Debut date", default=get_start_date)
-    end_date = fields.Datetime("End date", default=fields.Datetime.now)
+
+    debut_date = fields.Datetime("Du", default=get_start_date)
+    end_date = fields.Datetime("Au", default=fields.Datetime.now)
     type = fields.Selection([
         ('main_ouvre_glob', "Main courante global"), 
         ('main_ouvre_cais', "Main courante (Caissier)"), 
@@ -32,15 +41,26 @@ class PosConfigWizard(models.TransientModel):
     @api.multi
     def action_print(self):
         rapport = self.sudo().env.ref('pdp_reporting.report_main_courante')
-        if self.type == 'main_ouvre_glob':
+
+        # lines getted on report qweb by get_data_reporting method
+        if self.type in ['main_ouvre_glob', 'main_ouvre_cais']:
+            my_domaine = [
+                    ('config_id', '=', self.pos_config_id.id),
+                    ('date', '>=', self.debut_date),
+                    ('date', '<=', self.end_date),
+                    ]
+            if self.type == 'main_ouvre_cais':
+                my_domaine.append(('pos_vendeur_id', '=', self.cashier_id.id))
             py_lines = self.sudo().env['account.bank.statement.line'].read_group(domain=my_domaine,
                 fields=['amount_currency', 'amount'], groupby=['journal_id'])
 
-            pprint(orders)
-            rapport.report_action(orders, config=False)
-        elif self.type == 'main_ouvre_cais':
-            pass
+            return rapport.report_action(self)
+
         elif self.type == 'vente_eclat':
             pass
         elif self.type == 'vente_non_eclat':
             pass
+
+    def get_data_reporting(slef):
+        return []
+
